f8861488ab2fc2a3274224e42c7c8c8e0b9f2149
volodymyr.matseliukh <volodymyr.matselyukh@yandex.ru>
2022-08-02 16:28:49 +0300

    task 2 correctness

--- t2/t2 proj.tests/Task2Tests.cs
+++ t2/t2 proj.tests/Task2Tests.cs
@@ -6,6 +6,7 @@ namespace t2_proj.tests
     {
         [TestCase(new int[] { 2,1,1,3,2,1,1,3} , 3)]
         [TestCase(new int[] { 7,5,2,7,2,7,4,7}, 6)]
+        [TestCase(new int[] { 7, 1, 1, 7, 1, 1, 1, 1,  7 }, 2)]
         [TestCase(new int[] { 7, 3,7,3,1,3,4,1 }, 5)]
         [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, 8)]
         [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 1)]
--- t2/t2 proj/Task2.cs
+++ t2/t2 proj/Task2.cs
@@ -4,50 +4,99 @@
     {
         public int Solution(int[] A)
         {
+            var distinctNumbers = A.Distinct().ToList();
+            var distinctNumbersCount = distinctNumbers.Count();
 
-            if (A.Length == 1 || A.Length == 0) 
+            var startIndex = 0;
+            var endIndex = 0;
+            var shortestPath = A.Length;
+
+            for (int i = 0; i < A.Length; i++)
             {
-                return A.Length;
-            };
-
-            int result = A.Length;
-            bool proceedIteration = true;
-
-            int initialIndex = 0;
-            while (proceedIteration) 
-            {
-                int length = 0;
-                List<int> uniqueDigits = A.Distinct().ToList();
-                for (int i= initialIndex; i < A.Length; i++)
+                startIndex = i;
+                var neededNumbers = new List<int>(distinctNumbers);
+                for (int j = i; j < A.Length; j++)
                 {
-                    if (uniqueDigits.Count() > 0)
+                    if (neededNumbers.Count == 0)
                     {
-                        length++;
-                        if (uniqueDigits.Contains(A[i]))
-                        {
-                            uniqueDigits.Remove(A[i]);
-                        }
+                        break;
                     }
-                    else 
+
+                    var foundIndex = neededNumbers.IndexOf(A[j]);
+                    if (foundIndex > -1)
+                    { 
+                        neededNumbers.RemoveAt(foundIndex);
+                        endIndex = j;
+                    }
+                }
+
+                if (neededNumbers.Count == 0)
+                {
+                    if (endIndex - startIndex < shortestPath)
+                    {
+                        shortestPath = endIndex - startIndex + 1;
+                    }
+
+                    if (shortestPath == distinctNumbersCount)
                     {
                         break;
                     }
                 }
-                initialIndex++;
-
-                if ((initialIndex + uniqueDigits.Count() >= A.Length) || length == uniqueDigits.Count())
-                {
-                    proceedIteration = false;
-                 }
-
-                if (length < result && uniqueDigits.Count() == 0) 
-                {
-                    result = length;
-                }
-
             }
 
-            return  result;
+            return shortestPath;
         }
+        //public int Solution(int[] A)
+        //{
+
+        //    if (A.Length == 1 || A.Length == 0) 
+        //    {
+        //        return A.Length;
+        //    };
+
+        //    int result = A.Length;
+        //    bool proceedIteration = true;
+
+        //    int initialIndex = 0;
+
+
+
+        //    while (proceedIteration) 
+        //    {
+        //        int length = 0;
+
+        //        List<int> uniqueDigits = A.Distinct().ToList();
+
+        //        for (int i= initialIndex; i < A.Length; i++)
+        //        {
+        //            if (uniqueDigits.Count() > 0)
+        //            {
+        //                length++;
+        //                if (uniqueDigits.Contains(A[i]))
+        //                {
+        //                    uniqueDigits.Remove(A[i]);
+        //                }
+        //            }
+        //            else 
+        //            {
+        //                break;
+        //            }
+        //        }
+        //        initialIndex++;
+
+        //        if ((initialIndex + uniqueDigits.Count() >= A.Length) || length == uniqueDigits.Count())
+        //        {
+        //            proceedIteration = false;
+        //         }
+
+        //        if (length < result && uniqueDigits.Count() == 0) 
+        //        {
+        //            result = length;
+        //        }
+
+        //    }
+
+        //    return  result;
+        //}
     }
 }
